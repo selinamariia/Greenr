8629e740dc5842415205c1aa5c02c5855473fe1f
selinamariia <57037565+selinamariia@users.noreply.github.com>
2021-01-28 16:35:37 +0200

    Delete greenr directory

--- greenr/src/main/java/com/group41/greenr/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.group41.greenr.service;
-
-import com.group41.greenr.model.User;
-import com.group41.greenr.web.dto.UserRegistrationDto;
-
-public interface UserService {
-	User save(UserRegistrationDto registrionDto);
-
-}
