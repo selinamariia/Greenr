6e85b5f515358ad52c19b67a70395d8b68625136
Selina <mariiaselina@gmail.com>
2021-01-28 17:19:06 +0200

    new greenr project

---
+++ Greenr/src/test/java/com/group41/Greenr/GreenrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group41.Greenr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
