9e55aa8d03bbc61c1763e3871945ce55d2fa0f2b
Avneesh29 <73654123+Avneesh29@users.noreply.github.com>
2021-02-18 22:31:59 +0000

    Update MainController - Added Spring Security Feature
    Prevents User Going to Login when already logged in

--- Greenr/src/main/java/com/group41/Greenr/web/MainController.java
+++ Greenr/src/main/java/com/group41/Greenr/web/MainController.java
@@ -3,7 +3,10 @@ package com.group41.Greenr.web;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,7 +31,13 @@ public class MainController {
 	// @GetMapping is used to handle GET type of request method
 	@GetMapping("/login") 
 	public String login() {
-		return "login";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+		
+		return "redirect:/faq";
 	}
 	
 //	@GetMapping("/profile") 
