dbb2b19511ffd54d5c3be873e8d048eb9f93405d
magarb98 <35353062+magarb98@users.noreply.github.com>
2021-02-27 19:32:07 +0000

    Update MvcConfig.java
    EnabledWebMvc and added resource locations.

--- Greenr/src/main/java/com/group41/Greenr/configuration/MvcConfig.java
+++ Greenr/src/main/java/com/group41/Greenr/configuration/MvcConfig.java
@@ -2,23 +2,38 @@ package com.group41.Greenr.configuration;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
-public class MvcConfig implements WebMvcConfigurer{
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		exposeDirectory("user-photos", registry);
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+	public MvcConfig() {
+		super();
 	}
-	
+
+	@Override
+	public void addViewControllers(final ViewControllerRegistry registry) {
+	}
+
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**", "/css/**", "/resources/**", "/js/**", "/images/**").addResourceLocations(
+				"classpath:/static/css/", "classpath:/static/images/", "classpath:/static/js/",
+				"classpath:/resources/");
+		exposeDirectory("user-photos", registry);
+
+	}
+
 	private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
 		Path uploadDir = Paths.get(dirName);
 		String uploadPath = uploadDir.toFile().getAbsolutePath();
-		if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+		if (dirName.startsWith("../"))
+			dirName = dirName.replace("../", "");
 		registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/" + uploadPath + "/");
 	}
 }
