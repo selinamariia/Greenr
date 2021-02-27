c64b5b24f767bbb86c1ec6c4490d02df06d99725
magarb89 <magarb89@users.noreply.github.com>
2021-02-27 21:46:00 +0000

    reverted MvcConfig.java

--- Greenr/src/main/java/com/group41/Greenr/configuration/MvcConfig.java
+++ Greenr/src/main/java/com/group41/Greenr/configuration/MvcConfig.java
@@ -2,38 +2,22 @@ package com.group41.Greenr.configuration;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-
 @Configuration
-@EnableWebMvc
-public class MvcConfig implements WebMvcConfigurer {
-
-	public MvcConfig() {
-		super();
-	}
-
+public class MvcConfig implements WebMvcConfigurer{
+	
 	@Override
-	public void addViewControllers(final ViewControllerRegistry registry) {
-	}
-
-	@Override
-	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**", "/css/**", "/resources/**", "/js/**", "/images/**").addResourceLocations(
-				"classpath:/static/css/", "classpath:/static/images/", "classpath:/static/js/",
-				"classpath:/resources/");
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		exposeDirectory("user-photos", registry);
-
 	}
-
+	
 	private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
 		Path uploadDir = Paths.get(dirName);
 		String uploadPath = uploadDir.toFile().getAbsolutePath();
-		if (dirName.startsWith("../"))
-			dirName = dirName.replace("../", "");
+		if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
 		registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/" + uploadPath + "/");
 	}
 }
